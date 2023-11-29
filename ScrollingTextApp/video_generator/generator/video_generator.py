f0f3da51924f3d20025fd5a1f4c522fafe64fae6
dzherb <zherbin.dima@yandex.ru>
2023-11-29 18:42:36 +0300

    Make code more readable and add comments

--- ScrollingTextApp/video_generator/generator/video_generator.py
+++ ScrollingTextApp/video_generator/generator/video_generator.py
@@ -4,32 +4,30 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def create_scrolling_text(text, font_path, font_size,
-                                output_file='video/scrolling_text.mp4',
-                                width=100, height=100, fps=30,
-                                direction='left', duration=3):
+                          file_path='scrolling_text.mp4',
+                          image_width=100, image_height=100, fps=30,
+                          direction='left', duration=3):
     """
     Makes a video with scrolling text.
     Returns mp4 file.
     """
-    image_width = width
-    image_height = height
-    text_color = (255, 255, 255)  # Цвет текста
-    bg_color = (0, 50, 50)  # Цвет фона
+    text_color = (255, 255, 255)
+    bg_color = (0, 50, 50)
 
     font = ImageFont.truetype(font_path, font_size)
 
-    # Размеры текста в пикселях
+    # Get text size in pixels
     _, _, text_width, text_height = font.getbbox(text)
 
-    # Количество кадров
+    # Amount of frames
     num_frames = duration * fps
-    # Скорость смещения текста - (длина строки + двойная ширина кадра) / кол-во кадров
+    # Text speed
     speed = (text_width + 2*image_width) / num_frames
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    video_writer = cv2.VideoWriter(output_file, fourcc, fps, (image_width, image_height))
+    video_writer = cv2.VideoWriter(file_path, fourcc, fps, (image_width, image_height))
 
-    # Создаем кадры
+    # Make each frame and then unite all of them as a video
     for i in range(num_frames):
         img = Image.new("RGB", (image_width, image_height), bg_color)
         draw = ImageDraw.Draw(img)
@@ -42,17 +40,15 @@ def create_scrolling_text(text, font_path, font_size,
             raise ValueError("Invalid direction. Choose either 'left' or 'right'.")
 
         y = (image_height - text_height) // 2
-
         draw.text((x, y), text, font=font, fill=text_color)
 
-        # Запись кадра в итоговое видео
         frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
         video_writer.write(frame)
 
     video_writer.release()
 
 if __name__ == '__main__':
-    text = "Пример движущегося текста..."
+    text = "Scrolling text"
     font_path = "arial.ttf"
     font_size = 55
     duration = 3  # Продолжительность видео в секундах
--- ScrollingTextApp/video_generator/views.py
+++ ScrollingTextApp/video_generator/views.py
@@ -3,15 +3,20 @@ from django.http import HttpResponseNotFound, FileResponse
 from .generator.video_generator import create_scrolling_text
 
 def send_file(request):
-
-    text = request.GET.get('text')
-    file_location = 'video_generator/generator/video/scrolling_text.mp4'
-    create_scrolling_text(text, 'arial.ttf', font_size=55, output_file=file_location)
-
+    """Makes a video file from user's input and sends it back"""
     try:
+        # Getting 'text' parameter from url
+        text = request.GET.get('text')
+
+        # Generating video
+        file_location = 'video_generator/generator/video/scrolling_text.mp4'
+        create_scrolling_text(text, 'arial.ttf', font_size=55, file_path=file_location)
+
+        # Creating a response
         response = FileResponse(open(file_location, 'rb'))
         response['Content-Disposition'] = f'attachment; filename="{Path(file_location).stem}.mp4"'
-    except IOError:
-        response = HttpResponseNotFound('<h1>File does not exist, sorry :(</h1>')
+
+    except (IOError, TypeError):
+        response = HttpResponseNotFound('<h1>Something went wrong, sorry :(</h1>')
 
     return response
