6dc0fbb0d9a7e4b13447795c83954b7f1aeefef8
dzherb <zherbin.dima@yandex.ru>
2023-11-29 17:56:17 +0300

    Add sending file functionality

--- ScrollingTextApp/video_generator/views.py
+++ ScrollingTextApp/video_generator/views.py
@@ -2,14 +2,13 @@ from django.http import HttpResponse, HttpResponseNotFound, FileResponse
 
 def send_file(request):
 
-    file_location = '/generator/video/scrolling_text.mp4'
+    file_location = 'video_generator/generator/video/scrolling_text.mp4'
 
     try:
         response = FileResponse(open(file_location, 'rb'))
-        #response = HttpResponse(file_data, content_type='application/vnd.ms-excel')
         response['Content-Disposition'] = 'attachment; filename="scrolling_text.mp4"'
     except IOError:
         # handle file not exist case here
-        response = HttpResponseNotFound('<h1>File not exist</h1>')
+        response = HttpResponseNotFound('<h1>File does not exist</h1>')
 
     return response
