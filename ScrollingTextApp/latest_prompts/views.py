7b498a1543691588748d8ad00a503bee0ce6a380
dzherb <zherbin.dima@yandex.ru>
2023-12-01 11:41:06 +0300

    Add minor readability changes

--- ScrollingTextApp/latest_prompts/views.py
+++ ScrollingTextApp/latest_prompts/views.py
@@ -6,7 +6,6 @@ def latest_prompts(request):
     """Return page with list of latest prompts"""
     PROMPTS_ON_PAGE = 10
     prompts_to_show = Prompt.objects.all()[:PROMPTS_ON_PAGE]
-
     context = {'prompts': prompts_to_show,
                'max_prompts': PROMPTS_ON_PAGE,
                'amount_of_prompts': len(prompts_to_show)}
