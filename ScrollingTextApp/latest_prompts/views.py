3c2ef11e564584ba7add729d779ff9b27e0e491b
dzherb <zherbin.dima@yandex.ru>
2023-12-01 11:38:05 +0300

    Change main.html and views to show "latest prompts" more interactively

--- ScrollingTextApp/latest_prompts/views.py
+++ ScrollingTextApp/latest_prompts/views.py
@@ -5,6 +5,10 @@ from video_generator.models import Prompt
 def latest_prompts(request):
     """Return page with list of latest prompts"""
     PROMPTS_ON_PAGE = 10
+    prompts_to_show = Prompt.objects.all()[:PROMPTS_ON_PAGE]
 
-    context = {'prompts': Prompt.objects.all()[:PROMPTS_ON_PAGE]}
+    context = {'prompts': prompts_to_show,
+               'max_prompts': PROMPTS_ON_PAGE,
+               'amount_of_prompts': len(prompts_to_show)}
+
     return render(request,'latest_prompts/main.html', context=context)
